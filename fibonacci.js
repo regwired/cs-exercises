3091579de6e2b782c557b520dc82ff278a9c40c7
regwired <regwired@proton.me>
2022-11-04 13:01:05 +0100

    Create fibsRec function

--- fibonacci.js
+++ fibonacci.js
@@ -3,9 +3,14 @@
 const fibs = (n) => {
   const result = [];
   for (let i = 0; i < n; i++) {
-    (i < 2) ? result.push(i) : result.push(result[result.length - 1] + result[result.length - 2]);
+    i < 2 ? result.push(i) : result.push(result[result.length - 1] + result[result.length - 2]);
   }
   return result;
 }
 
 console.log(fibs(8));
+
+
+const fibsRec = (n) => n == 2 ? [0, 1] : [...fibsRec(n - 1), fibsRec(n - 1)[n-2] + fibsRec(n - 1)[n - 3]];
+
+console.log(fibsRec(8));
